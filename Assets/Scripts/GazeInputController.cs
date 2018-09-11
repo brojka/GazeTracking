8ffcfbf03cc2ae51eff28c9bf1ca4a1873d63cb7
Brojka.hr - Krunoslav Gregorec <kruno@brojka.hr>
2018-09-11 11:05:31 +0200

    Initial project file commit. Git ignore file update.

---
+++ Assets/Scripts/GazeInputController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum DirectionX8 { Center, Down, LeftDown, Left, LeftUp, Up, RightUp, Right, RightDown}
+
+public class GazeInputController : MonoBehaviour
+{
+    [SerializeField] VisageTracker _tracker; 
+    [SerializeField] float _horizontalLookAtScale;
+    [SerializeField] float _horizontalLookAtLimit;
+    [SerializeField] float _verticalLookAtScale;
+    [SerializeField] float _verticalLookAtLimit;
+    [SerializeField] float _currentHorizontalValue;
+    [SerializeField] float _currentVerticalValue;
+    [SerializeField] float _detectionThresholdOffset;
+
+
+
+    public float CurrentVerticalValue
+    {
+        get
+        {
+            return _currentVerticalValue;
+        } 
+    }
+    public float CurrentHorizontalValue
+    {
+        get
+        {
+            return _currentHorizontalValue;
+        } 
+    }
+
+    private void Start()
+    {
+        StartCoroutine(DetectGazeDirection());
+    }
+
+    private IEnumerator DetectGazeDirection()
+    {
+        while (true)
+        {
+            _currentHorizontalValue = Mathf.Clamp(_tracker.GazeDirection.x * _horizontalLookAtScale, -_horizontalLookAtLimit, _horizontalLookAtLimit);
+            _currentVerticalValue = Mathf.Clamp(_tracker.GazeDirection.y * _verticalLookAtScale, -_verticalLookAtLimit, _verticalLookAtLimit);
+
+            yield return null;
+        }
+    } 
+}
---
+++ Assets/Scripts/GazeReactionController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GazeReactionController : MonoBehaviour
+{
+    [SerializeField] GazeInputController _gazeInputController;
+    [SerializeField] GameObject _objectToManipulate;
+    [SerializeField] float _verticalScale = 1f;
+    [SerializeField] float _horizontalScale = 1f;
+    [SerializeField] float _horizontalPositionLimit = 10f;
+    [SerializeField] float _verticalPositionLimit = 5f;
+    [SerializeField] float _moveSpeed = 10f;
+    [SerializeField] float _movementActivationMinimumDistance = 3f;
+    [SerializeField] bool _invertAxisX = false;
+    [SerializeField] bool _invertAxisY = false;
+    [SerializeField] UnityEngine.UI.Text _text;
+    [SerializeField] GameObject _gazePointingProp;
+    [SerializeField] float _offset = 10f;
+
+
+    private void Start()
+    {
+        if (_gazeInputController == null)
+            _gazeInputController = FindObjectOfType<GazeInputController>();
+        if (_objectToManipulate == null)
+            _objectToManipulate = gameObject;
+         
+
+        StartCoroutine(OnGazeDetected());
+    }
+
+    private float a = 0f;
+    private float b = 0f; 
+
+    private IEnumerator OnGazeDetected()
+    {
+        while (true)
+        {
+            if (_gazeInputController == null || _objectToManipulate == null)
+                yield break;
+       
+            _objectToManipulate.transform.localRotation = Quaternion.AngleAxis(-_gazeInputController.CurrentHorizontalValue, Vector3.up) * Quaternion.AngleAxis(-_gazeInputController.CurrentVerticalValue, Vector3.right);
+               
+            a = _gazeInputController.CurrentHorizontalValue;
+            b = _gazeInputController.CurrentVerticalValue;
+
+
+            yield return new WaitWhile(() => Mathf.Abs(_gazeInputController.CurrentHorizontalValue - a) < _offset || Mathf.Abs(_gazeInputController.CurrentVerticalValue - b) < _offset);
+
+            yield return null;
+        }
+    }
+
+  /*  private IEnumerator MoveRoutine()
+    {
+        while (Mathf.Abs(Vector3.Distance(_targetPosition, _objectToManipulate.transform.position)) > _movementActivationMinimumDistance)
+        {
+            _objectToManipulate.transform.position = Vector3.MoveTowards(_objectToManipulate.transform.position, _targetPosition, _moveSpeed * Time.deltaTime);
+            Debug.Log("KREĆEM SE!");
+            yield return null;
+        }
+    }*/
+}
