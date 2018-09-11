9cb103a2aee3b3dac5a73dd5c18d31323b68a7d3
Brojka.hr - Krunoslav Gregorec <kruno@brojka.hr>
2018-09-11 19:50:11 +0200

    Daily feature testing.

---
+++ Assets/Scripts/HeadInputController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadInputController : MonoBehaviour
+{
+    [SerializeField] VisageTracker _tracker;
+    [SerializeField] GameObject _cube;
+    [SerializeField] float _speed = 10f;
+
+    private void Update()
+    {
+        if (_tracker.Rotation.y > -180f)
+            _cube.transform.Rotate(Vector3.up * Time.deltaTime * _speed);
+        else
+            _cube.transform.Rotate(Vector3.up * Time.deltaTime * -_speed);
+    }
+
+}
